a2a30cb50cc66d4c88834011336c02d28de14161
aqib akram <59762296+aqib-ui@users.noreply.github.com>
2024-09-25 17:33:01 +0400

    update

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const assignmentSubmissionRoutes = require('./routes/assignment-submission-route
 const feesInvoicesRoutes = require('./routes/fee-routes')
 
 const cors = require('cors');
+app.use(express.static(__dirname + ''));
 app.use(cors());
 app.use(bodyParser.json({ limit: '250mb' }));
 app.use(bodyParser.urlencoded({ limit: '250mb', extended: true }));
